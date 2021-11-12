24c8bbb63d70107ac423c2396420282dc8e8fc2c
yuly <mayhuirevargasyuly@cldvarica.cl>
2021-11-12 10:57:33 -0300

    Requerimiento 1
    se termino el requerimiento 1 modificando el footer

--- interna/plantillas/headerHTML.php
+++ interna/plantillas/headerHTML.php
@@ -3,7 +3,7 @@
     <div class="banner">
       <table>
         <tr>
-          <td width="198"><a href="/ServTec/"><img src="/ServTec/imagen/bit2.png" alt="BIT Soluciones Informáticas"></a></td>
+          <td width="198"><a href="/ServTec/"><img class="banner-img" src="/ServTec/imagen/banner01.png" alt="BIT Soluciones Informáticas"></a></td>
           <td width="914" align="right">
             <!-- LOGIN DE ACCESO A USUARIO -->
             <form id="form1" name="form1" method="post" action="/ServTec/interna/procesar_usuario.php">
@@ -11,7 +11,7 @@
                 <tr>
                   <td width="88" class="login" align="right"><label>Usuario:</label></td>
                   <td width="63">
-                    <input name="usuario" size="5" type="text" id="usuario" maxlength="15" />
+                    <input name="Usuario" size="5" type="text" id="Usuario" maxlength="15" />
                   </td>
                   <td width="52" rowspan="2">
                     <a href="#" onClick="ingresar();" onMouseOver="image1.src='/ServTec/imagen/ingresar2.png';" onMouseOut="image1.src='/ServTec/imagen/ingresar1.png';">
@@ -32,9 +32,7 @@
       <ul>
         <li><a href="/PagServTecProfesor/">Inicio</a></li>
         <li><a href="/PagServTecProfesor/interna/servicios.php">Servicios</a></li>
-        <li><a href="/PagServTecProfesor/interna/proyectos.php">Proyectos</a></li>
-        <li><a href="/PagServTecProfesor/interna/nosotros.php">Nosotros</a></li>
-        <li><a href="/PagServTecProfesor/interna/contacto.php">Contacto</a></li>
+        <li><a href="/PagServTecProfesor/interna/proyectos.php">Solicitud de servicios </a></li>
       </ul>
     </nav>
   </div>
